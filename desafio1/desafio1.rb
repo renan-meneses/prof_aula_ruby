525df58d573fee7a8500fffc381b8dbba9bb4e3e
Renan <renanmenesesdev@gmail.com>
2024-05-30 15:16:08 -0300

    feat: subindo desafio

---
+++ desafio1/desafio1.rb
@@ -0,0 +1,8 @@
+print "Nome*: "
+name = gets.chomp
+
+print "Sobrenome: "
+lastname = gets.chomp
+print "Idade: "
+age = gets.chomp
+puts "Olá #{name} #{lastname}! Seja bem vindo! Sua idade é #{age}."
